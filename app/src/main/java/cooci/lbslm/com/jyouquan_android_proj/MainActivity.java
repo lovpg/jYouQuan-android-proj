a93b665995ffe563b65dea54dd0ac4ffe1b4afc6
gaozl <gaozl@olla.im>
2016-09-26 10:52:14 +0800

    初始化Android项目

---
+++ app/src/main/java/cooci/lbslm/com/jyouquan_android_proj/MainActivity.java
@@ -0,0 +1,13 @@
+package cooci.lbslm.com.jyouquan_android_proj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
